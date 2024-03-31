24071251b69163f25376c182404fc6b0bcfa42a9
Bhaskar <bhaskarmulik27@gmail.com>
2024-03-31 10:19:47 +0530

    Making a sep file for ref codes

---
+++ .history/notebooks/ref_code_20240331101913.py
@@ -0,0 +1,25 @@
+############Code 1#######################
+import numpy as np
+
+# Assuming 'data' is your speech signal array and 'fs' is the sampling frequency
+frame_size = 0.32  # 320 ms
+overlap = 0.5
+frame_samples = int(frame_size * fs)
+step_size = int(frame_samples * (1 - overlap))
+
+# Function to compute the Lp norm
+def lp_norm(data, p):
+    return (np.sum(np.abs(data)**p))**(1/p)
+
+# Compute the L6 norm over split-second intervals
+l6_norms = []
+for i in range(0, len(data) - frame_samples + 1, step_size):
+    frame = data[i:i+frame_samples]
+    l6_norm = lp_norm(frame, 6)
+    l6_norms.append(l6_norm)
+
+# Compute the L2 norm over all split-second L6 norms
+l2_norm_over_l6 = lp_norm(np.array(l6_norms), 2)
+
+print(f'L6 norms over split-second intervals: {l6_norms}')
+print(f'L2 norm over all split-second L6 norms: {l2_norm_over_l6}')
